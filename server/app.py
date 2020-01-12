03e2278055298e7897be6807d3794f6527f30223
Farshad E <farshade@Farshads-MacBook-Pro.local>
2020-01-12 00:03:29 -0600

    modified app.py to with the correct route api to return MongoDB data in jsonify format

--- server/app.py
+++ server/app.py
@@ -13,14 +13,38 @@ client = pymongo.MongoClient(conn)
 db = client.traffic_stops_db
 collection = db.traffic_stops
 
-
-@app.route("/trafficdata.json")
-def index():
-    # write a statement that finds all the items in the db and sets it to a variable
-    traffic_stops = list(db.traffic_stops.find())
-    attributes = ["Year", "Date", "Race", "Gender", "DriverSearched", "VehicleSearched", "Citation", "Reason", "Grid", "Location"]
-    output = [{atr: traffic_stop[atr] for atr in attributes} for traffic_stop in traffic_stops]
-    return json.dumps(output)
+#**********************************
+#        Set up the routes         
+#**********************************
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"<br>Available Routes:<br/>"
+        f"<br>/trafficdata<br/>"
+        f"<br>/trafficdata100<br/>"
+        )   
+# All the traffic stop data
+@app.route("/trafficdata")
+def trafficdata():
+    # Need to handle the issue with the _id field in MongoDB 
+    # It is throwing an error in jsonify
+    stops = []
+    for stop in db.traffic_stops.find():
+        stop.pop('_id')
+        stop.pop('Unnamed: 0')
+        stops.append(stop)
+    return jsonify(stops)
+# limit to only the 100 records
+@app.route("/trafficdata100")
+def trafficdata100():
+    #Limiting the results to 100 for testing due to latency
+    stops = []
+    for stop in db.traffic_stops.find().limit(100):
+        stop.pop('_id')
+        stop.pop('Unnamed: 0')
+        stops.append(stop)
+    return jsonify(stops)
 
 if __name__ == "__main__":
     app.run(debug=True)
