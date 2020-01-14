e0dddc6837ca9979251f34ae7b3c1e9b50d3d524
Farshad E <farshade@stp-er-10-129-188-60.wireless.umn.edu>
2020-01-13 20:51:38 -0600

    Changes made: (1)restructred folders; (2)changes to app.py routes to pass data to html

---
+++ static/dashboard.js
@@ -0,0 +1,6 @@
+console.log ("load dashboard.js");
+
+d3.json("/trafficdata100").then(function (data){
+    console.log (data);
+    d3.selectAll('.dashboard').text('We made it to the js file!');
+});
